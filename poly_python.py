a23715e6a8f7e5638e65e3a66d89f725134872b7
Lucas Page-Caccia <lucas.page-caccia@mail.mcgill.ca>
2023-07-27 06:34:43 -0700

    ok good start

--- poly_python.py
+++ poly_python.py
@@ -6,7 +6,7 @@ DTYPE = torch.float32
 mixing_weights_ptr, weights_ptr, input_ptr, output_ptr = 0, 0, 0, 0
 
 # Matrix dimensions
-bs, n_skills, rank, d_in, d_out = 16, 6, 12, 129, 256
+bs, n_skills, rank, d_in, d_out = 32, 8, 16, 256, 256
 
 # generate weights a la polytropon
 module_logits = torch.randn(bs, n_skills, dtype=DTYPE, device='cuda')
@@ -16,10 +16,10 @@ input_ = torch.randn(bs, d_in, dtype=DTYPE, device='cuda')
 output_ = torch.empty(bs, rank, dtype=DTYPE, device='cuda')
 
 # meta parameters
-block_size_m = 5 
-block_size_n = 7
-block_size_k = 3
-group_size_m = 4
+BLOCK_SIZE_M = 16 # 5 
+BLOCK_SIZE_N = 16 # 7
+BLOCK_SIZE_K = 16 # 3
+GROUP_SIZE_M = 16 # 4
 
 A = torch.einsum("bs,sdr->bdr", (mixing_weights, skill_weights))
 A = A.reshape(bs, d_in, rank)
@@ -61,24 +61,24 @@ def tl_store(container, idx, values, mask=None):
     return container
 
 # simulate launch grid
-for pid in range(ceil_div(M, block_size_m) * ceil_div(N, block_size_n)):
+for pid in range(ceil_div(M, BLOCK_SIZE_M) * ceil_div(N, BLOCK_SIZE_N)):
 
     # simulate a kernel call
     if use_groups:
         # get better l2-cache utilization by grouping
-        num_pid_m = ceil_div(M, block_size_m)
-        num_pid_n = ceil_div(N, block_size_n)
-        num_pid_in_group = group_size_m * num_pid_n
+        num_pid_m = ceil_div(M, BLOCK_SIZE_M)
+        num_pid_n = ceil_div(N, BLOCK_SIZE_N)
+        num_pid_in_group = GROUP_SIZE_M * num_pid_n
         group_id = pid // num_pid_in_group
-        first_pid_m = group_id * group_size_m
-        group_size_m = min(num_pid_m - first_pid_m, group_size_m)
-        pid_m = first_pid_m + (pid % group_size_m)
-        pid_n = (pid % num_pid_in_group) // group_size_m
+        first_pid_m = group_id * GROUP_SIZE_M
+        GROUP_SIZE_M = min(num_pid_m - first_pid_m, GROUP_SIZE_M)
+        pid_m = first_pid_m + (pid % GROUP_SIZE_M)
+        pid_n = (pid % num_pid_in_group) // GROUP_SIZE_M
         print(f'pid_m: {pid_m}, pid_n: {pid_n}')
         pass
     else:
         # each pid computes one (i,j) / (m, n) entry in the final matrix
-        num_blocks_n = ceil_div(N, block_size_n)
+        num_blocks_n = ceil_div(N, BLOCK_SIZE_N)
         pid_m = pid // num_blocks_n
         pid_n = pid % num_blocks_n
 
@@ -86,50 +86,62 @@ for pid in range(ceil_div(M, block_size_m) * ceil_div(N, block_size_n)):
 
     # compute the offsets into the input matrices
     # this is exactly correct if strides for this dimension are 1. 
-    offset_am =  pid_m * block_size_m + torch.arange(block_size_m) # this is the input. So ok 
-    offset_bn =  pid_n * block_size_n + torch.arange(block_size_n) # this is the weights
-    offset_k = torch.arange(block_size_k)
+    offset_am =  pid_m * BLOCK_SIZE_M + torch.arange(BLOCK_SIZE_M) # this is the input. So ok 
+    offset_bn =  pid_n * BLOCK_SIZE_N + torch.arange(BLOCK_SIZE_N) # this is the weights
+    offset_k = torch.arange(BLOCK_SIZE_K)
+    offset_skills = torch.arange(n_skills)
 
     # account for strides
-    # (block_size_m, block_size_k) [These are 2-D indices for X
+    # (BLOCK_SIZE_M, BLOCK_SIZE_K) [These are 2-D indices for X
     # we will progressively move the k index to get the full row
     # this is the input. so ok
-    X_ptr = input_ptr + input_.stride(0) * offset_am[:, None] + input_.stride(1) * offset_k[None, :]
+    X_ptr = input_ptr + offset_am[:, None] * input_.stride(0) + offset_k[None, :] * input_.stride(1)
 
     # this is the weight tensor
-    offset_skills = torch.arange(n_skills)
-    Y_ptr = weights_ptr + skill_weights.stride(2) * offset_bn[None, None, :] + \
-                          skill_weights.stride(1) * offset_k[None, :, None] + \
-                          skill_weights.stride(0) * offset_skills[:, None, None]
+    Y_ptr = weights_ptr + skill_weights.stride(2) * offset_bn[None, :] + \
+                          skill_weights.stride(1) * offset_k[:, None] # + \
+                          # skill_weights.stride(0) * offset_skills[:, None, None]
 
     # additionally, we have to index the mixing_coefs. Here the batch size is the first (index 0) dim.
-    offset_w = pid_m * block_size_m + torch.arange(n_skills)
-    offset_w = torch.arange(n_skills)
-    mix_ptr = mixing_weights_ptr + mixing_weights.stride(0) * offset_am[:, None] + mixing_weights.stride(1) * offset_w[None, :]
+    mix_ptr = mixing_weights_ptr + mixing_weights.stride(0) * offset_am[:, None] # + mixing_weights.stride(1) * offset_w[None, :]
 
-    accumulator = torch.zeros((block_size_m, block_size_n), dtype=input_.dtype, device=input_.device)
-    # for k_chunk in range(0, K, block_size_k):
-    for k_chunk in range(0, ceil_div(K, block_size_k)): # makes the masking of invalid outputs easier
+    accumulator = torch.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=input_.dtype, device=input_.device)
+    
+    # load the block's mixing coefs here, as they do not depend on K
+    mw_mask  = (offset_am[:, None] < M) 
+    
+    # for k_chunk in range(0, K, BLOCK_SIZE_K):
+    for k_chunk in range(0, ceil_div(K, BLOCK_SIZE_K)): # makes the masking of invalid outputs easier
 
-        # simulate 1d indexing as in triton                               # actual shapes          # dimensions being indexed
-        try:
-            x_mask   = (offset_am[:, None]  < M) & (offset_k[None, :] < K - k_chunk * block_size_k)
-            ws_mask  = (offset_k[None, :, None] < K - k_chunk * block_size_k) & (offset_bn[None, None, :] < N)
-            mw_mask  = (offset_am[:, None]  < M)  
-            x_chunk  = tl_load(input_, X_ptr, mask=x_mask)            # BSM, BSK               # bs,     d_in         (M, K)
-            ws_chunk = tl_load(skill_weights, Y_ptr, mask=ws_mask)    # n_skills, BSN, BSK     # skills, d_in, d_out  (_, K, N)
-            mw_chunk = tl_load(mixing_weights, mix_ptr, mask=mw_mask) # BSM, n_skills          # bs,     skills       (M, _)
-        except IndexError as e:
-            breakpoint()
-            xx = 1
+        x_mask   = (offset_am[:, None]  < M) & (offset_k[None, :] < K - k_chunk * BLOCK_SIZE_K)
+        ws_mask  = (offset_k[:, None] < K - k_chunk * BLOCK_SIZE_K) & (offset_bn[None, :] < N)
+
+        # simulate 1d indexing as in triton                       # actual shapes          # dimensions being indexed
+        x_chunk  = tl_load(input_, X_ptr, mask=x_mask)            # BSM, BSK               # bs,     d_in         (M, K)
+        ws_chunk = tl_load(skill_weights, Y_ptr, mask=ws_mask)    # n_skills, BSN, BSK     # skills, d_in, d_out  (_, K, N)
+        mw_chunk = tl_load(mixing_weights, mix_ptr, mask=mw_mask) # BSM, n_skills          # bs,     skills       (M, _)
 
         # let's just iterate over the skills for now
         for skill_idx in range(n_skills):
-            accumulator += mw_chunk[:, skill_idx, None] * torch.matmul(x_chunk, ws_chunk[skill_idx])
+            skill_weight_ptr = Y_ptr + skill_weights.stride(0) * skill_idx
+            skill_chunk = tl_load(skill_weights, skill_weight_ptr, mask=ws_mask).view(BLOCK_SIZE_K, BLOCK_SIZE_N)
+            skill_mix_ptr = mix_ptr + mixing_weights.stride(1) * skill_idx 
+            skill_weight = tl_load(mixing_weights, skill_mix_ptr, mask=mw_mask)
+            # accumulator += mw_chunk[:, skill_idx, None] * torch.matmul(x_chunk, ws_chunk[skill_idx])
+            accumulator += skill_weight * torch.matmul(x_chunk, skill_chunk)
 
+        '''
+        ws_rshp = ws_chunk.view(n_skills, -1)
+        merged_weights = torch.matmul(mw_chunk, ws_rshp)
+        merged_weights = merged_weights.reshape(BLOCK_SIZE_M, BLOCK_SIZE_K, BLOCK_SIZE_N)
+        x_rshp = x_chunk.view(BLOCK_SIZE_M, 1, BLOCK_SIZE_K)
+        out = torch.matmul(x_rshp, merged_weights).reshape(BLOCK_SIZE_M, BLOCK_SIZE_N)
+        accumulator += out
+        '''
+        
         # move pointers
-        X_ptr += block_size_k * input_.stride(1)
-        Y_ptr += block_size_k * skill_weights.stride(1)
+        X_ptr += BLOCK_SIZE_K * input_.stride(1)
+        Y_ptr += BLOCK_SIZE_K * skill_weights.stride(1)
 
     # expected_tmp = torch.zeros_like(accumulator)
     # x_chunk = input_[offset_am]
@@ -138,8 +150,8 @@ for pid in range(ceil_div(M, block_size_m) * ceil_div(N, block_size_n)):
     # breakpoint()
 
     # compute the offsets into the output matrix (think of this as 1D-indexing)
-    offset_om = pid_m * block_size_m + torch.arange(block_size_m)
-    offset_on = pid_n * block_size_n + torch.arange(block_size_n)
+    offset_om = pid_m * BLOCK_SIZE_M + torch.arange(BLOCK_SIZE_M)
+    offset_on = pid_n * BLOCK_SIZE_N + torch.arange(BLOCK_SIZE_N)
 
     # account for strides
     O_ptr = output_ptr + output_.stride(0) * offset_om[:, None] + output_.stride(1) * offset_on[None, :]
@@ -148,7 +160,5 @@ for pid in range(ceil_div(M, block_size_m) * ceil_div(N, block_size_n)):
     output_mask = (offset_om[:, None] < M) & (offset_on[None, :] < N)
     tl_store(output_, O_ptr, accumulator, mask=output_mask)
 
-breakpoint() 
-expected = torch.matmul(X, Y)
-assert torch.allclose(expected, output), breakpoint()
-y = 1
+print(f'difference total : {(exp_out - output_).abs().sum().item():.8f}')
+print(f'difference max   : {(exp_out - output_).abs().max().item():.8f}')
